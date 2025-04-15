85c1420ad009ab9b84b73e3215a94fb43d233570
kalyan199999 <gottapupavankalyan@gmail.com>
2025-04-16 00:07:30 +0530

    project-2 added

--- PROJECTS/project_2/index.js
+++ PROJECTS/project_2/index.js
@@ -1,15 +1,25 @@
 const open = document.querySelector(".open-btn");
-const close = document.querySelector(".close-btn");
+// const close = document.querySelector(".close-btn");
 
 const openMenu = ()=>{
     const overlay = document.querySelector(".overlay");
-    overlay.classList.add('overlay--active')
+
+    if(open.innerText === "Open"){
+        open.innerText = "Close"
+        open.style.backgroundColor = "red"
+    }
+    else{
+        open.innerText = "Open"
+        open.style.backgroundColor = "green"
+    }
+    
+    overlay.classList.toggle('overlay--active')
 }
 
-const closeMenu = ()=>{
-    const overlay = document.querySelector(".overlay");
-    overlay.classList.remove('overlay--active')
-}
+// const closeMenu = ()=>{
+//     const overlay = document.querySelector(".overlay");
+//     overlay.classList.remove('overlay--active')
+// }
 
 open.addEventListener('click', openMenu);
-close.addEventListener('click', closeMenu);
+// close.addEventListener('click', closeMenu);
