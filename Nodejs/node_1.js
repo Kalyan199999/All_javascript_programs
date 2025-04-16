5d60d189e71818843b8047c872786762276f056f
kalyan199999 <gottapupavankalyan@gmail.com>
2025-04-16 16:53:28 +0530

    node-js initialized

---
+++ Nodejs/node_1.js
@@ -0,0 +1,39 @@
+// HTTP module
+
+const http = require('http')
+const fs = require('fs')
+
+
+const server = http.createServer( (req, res)=>{
+    console.log("Request from the user");
+
+    // console.log(req.headers);
+    console.log(req.url);
+
+    const logData = Date.now() + " " + req.url + "\n"
+
+    fs.appendFile( "./logs.txt" , logData , (err,data)=>{
+        if(err){
+            console.log(err);
+        }
+    } )
+
+    switch (req.url) {
+        case "/":
+            res.end("This is the home page")
+            break;
+        case "/about":
+            res.end("This is the about page")
+            break;
+    
+        default:
+            res.end("Unknown page")
+            break;
+    }
+    
+})
+
+
+server.listen(5050, ()=>{
+    console.log(`Server is running on port localhost:5050`);
+})
